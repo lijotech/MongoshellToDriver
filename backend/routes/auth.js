db9b12abb33c20fe38d5395563ac265dfd3af39a
lijotech <lijotech@gmail.com>
2023-01-21 23:06:05 +0400

    User Sign up and Login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const Router = require('express').Router;
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-
+const db = require('../db');
 const router = Router();
 
 const createToken = () => {
@@ -13,11 +13,30 @@ router.post('/login', (req, res, next) => {
   const pw = req.body.password;
   // Check if user login is valid
   // If yes, create token and return it to client
-  const token = createToken();
-  // res.status(200).json({ token: token, user: { email: 'dummy@dummy.com' } });
-  res
+  db.getDb().db().collection('users').findOne({email:email})
+  .then(userDoc=>{
+    return bcrypt.compare(pw,userDoc.password);
+  })
+  .then(result=>{
+    if (!result)
+    {
+      throw Error();
+    }
+    console.log(result);
+    const token = createToken();
+    res
+    .status(200)
+    .json({ message: 'Authentication Succeeded.' ,token:token});
+  })
+  .catch(err=>{
+    res
     .status(401)
     .json({ message: 'Authentication failed, invalid username or password.' });
+  });
+
+  
+  // res.status(200).json({ token: token, user: { email: 'dummy@dummy.com' } });
+
 });
 
 router.post('/signup', (req, res, next) => {
@@ -29,10 +48,22 @@ router.post('/signup', (req, res, next) => {
     .then(hashedPW => {
       // Store hashedPW in database
       console.log(hashedPW);
-      const token = createToken();
-      res
-        .status(201)
-        .json({ token: token, user: { email: 'dummy@dummy.com' } });
+
+      db.getDb().db()
+        .collection('users')
+        .insertOne({ email: email, password: hashedPW })
+        .then(result => {
+          console.log(result);
+          const token = createToken();
+          res
+            .status(201)
+            .json({ token: token, user: { email: email } });
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json({ message: 'An error occurred.' });
+        });
+
     })
     .catch(err => {
       console.log(err);
